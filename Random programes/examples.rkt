36324620e2db4f3142961629025bc7ee03080f86
fine-man <spacetime299@gmail.com>
2021-08-02 17:06:27 +0530

    added exercise 1.2

---
+++ Random programes/examples.rkt
@@ -0,0 +1,14 @@
+;; The first three lines of this file were inserted by DrRacket. They record metadata
+;; about the language level of this file in a form that our tools can easily process.
+#reader(lib "htdp-beginner-reader.ss" "lang")((modname examples) (read-case-sensitive #t) (teachpacks ()) (htdp-settings #(#t constructor repeating-decimal #f #t none #f () #f)))
+(define (square x)
+  (* x x))
+
+(define (sum-of-squares x y)
+  (+ (square x)
+     (square y)))
+
+(define (ab x)
+  (if (< x 0)
+      (- (sum-of-squares x x))
+      (square x)))
