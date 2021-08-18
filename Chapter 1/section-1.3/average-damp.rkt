1b36419cee5b8366b7607befe9fe5c9a4391e11a
fine-man <spacetime299@gmail.com>
2021-08-18 19:54:28 +0530

    added average-damp

---
+++ Chapter 1/section-1.3/average-damp.rkt
@@ -0,0 +1,36 @@
+#lang sicp
+
+; Helper Procedures
+(define (average a b)
+  (/ (+ a b) 2))
+
+(define (square x)
+  (* x x))
+
+(define tolerance 0.0001)
+
+(define (close-enough? a b)
+  (< (abs (- a b)) tolerance))
+
+
+(define (fixed-point f new-guess)
+  (define (try guess)
+    (let ((next (f guess)))
+      (if (close-enough? guess next)
+          next
+          (try next))))
+  (try new-guess))
+
+; Average-damp Procedure
+(define (average-damp f)
+  (lambda (x) (average x (f x))))
+
+(define (sqrt x)
+  (fixed-point
+   (average-damp (lambda (y) (/ x y)))
+   1.0))
+
+(define (cube-root x)
+  (fixed-point
+   (average-damp (lambda (y) (/ x (square y))))
+   1.0))
